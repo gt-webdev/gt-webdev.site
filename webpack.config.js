9764d612fc2c81f4c01d973fa2229bf3d561c815
Saleh Hamadeh <saleh@Salehs-Macintosh.local>
2016-01-30 22:09:41 -0500

    Remove unused dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,6 @@ var path = require('path');
 var webpack = require('webpack');
  
 module.exports = {
-  entry: './src/pages/home.js',
-  output: { path: __dirname, filename: './dist/bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules|dist/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
+  entry: './dist-server/pages/home.js',
+  output: { path: __dirname, filename: './dist/bundle.js' }
 };
