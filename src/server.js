c413534f94df9e338e0de01aa37a68b98e9a1535
Saleh Hamadeh <saleh@Salehs-Macintosh.local>
2016-02-07 13:39:35 -0500

    Use process.env.PORT.

--- src/server.js
+++ src/server.js
@@ -36,6 +36,7 @@ app.use(express.static('../dist'));
 // Serve other public files
 app.use(express.static('../public'));
 
-app.listen(3000, function() {
-  console.log("Listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on port " + port);
 });
