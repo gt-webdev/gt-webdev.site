f69a42002507c460b1d07c7d4be9792d3ea0eef4
Saleh Hamadeh <saleh@Salehs-Macintosh.local>
2016-02-07 12:36:36 -0500

    Fix client-side routing.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,16 @@
 import ReactDOM from 'react-dom'
 import React from 'react';
+import {Link} from 'react-router'
 
 class Navbar extends React.Component {
   render() {
     return (
       <ul>
-        <li>Home</li>
-        <li>Schedule</li>
-        <li>Members</li>
-        <li>Blog</li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/learn">Learn</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     );
   }
--- src/router.js
+++ src/router.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { browserHistory, Router, Route, Link } from 'react-router'
+import {render} from 'react-dom'
+import { Router, Route, Link } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 import Home from './pages/home'
 import Events from './pages/events'
 
-module.exports = (
-  <Router history={browserHistory}>
+// Used for client-side and server-side rendering
+var router = (
+  <Router history={typeof document !== "undefined" ? createBrowserHistory() : null}>
     <Route path="/" component={Home} />
-    <Route path="/events" component={Events} />
+    <Route path="events" component={Events} />
   </Router>
 );
+
+// Attach handlers when page is opened in browser
+if (typeof document !== "undefined") {
+  document.addEventListener('DOMContentLoaded', function() {
+    render(router, document.getElementById("root"));
+  });
+}
+
+module.exports = router;
