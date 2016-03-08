c98ba460d9242c82bd374f6206a9fccdf1224f92
Saleh Hamadeh <saleh@lawn-143-215-127-143.lawn.gatech.edu>
2016-03-08 17:18:40 -0500

    Add personal website icon and fix links styles.

--- src/components/team.js
+++ src/components/team.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 import officers from '../data/officers.js'
 
 function getSocialIcons(officer) {
-  return _.chain(['twitter', 'github', 'linkedin'])
+  return _.chain(['website', 'twitter', 'github', 'linkedin'])
     .filter((site) => {
       return typeof officer[site] != 'undefined';
     })
