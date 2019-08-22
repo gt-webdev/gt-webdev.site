380567e34d6a699797e20ebeb821ae43c8e9f85d
John Thompson <jthompson4004@gmail.com>
2019-08-22 12:35:26 -0400

    trying

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.scss';
+import './scss/styles.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
